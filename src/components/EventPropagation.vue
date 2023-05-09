aa3f8f47e74eca608c8d2813501f6259f396b0a3
Antonin Avon <aavon@axome.com>
2023-05-09 15:00:28 +0200

    fun stuff

---
+++ src/components/EventPropagation.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+function userClick(from: string) {
+    console.log('click from', from);
+}
+
+function formSubmit() {
+    console.log('Page non rechargé');
+}
+
+function escapeUP() {
+    console.log('escape pressed');
+}
+</script>
+
+<template>
+	<div @click="userClick('div')">
+		<h2 @click.stop="userClick('h2')">Hello World!</h2>
+	</div>
+	<form @submit.prevent="formSubmit">
+		<button>Submit</button>
+	</form>
+	<input @keyup.escape="escapeUP">
+</template>
---
+++ src/components/State.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import {reactive} from "vue";
+
+const state = reactive({
+    user: {
+        name: 'Toto',
+        age: 18
+    },
+    count: 0
+})
+function incrementCount() {
+    state.count++;
+}
+function decreaseCount() {
+    state.count--;
+}
+
+function toggleUserInfo() {
+    user.name = user.name === 'Toto' ? 'Antonin' : 'Toto';
+    user.age = user.age === 18 ? 28 : 18;
+}
+
+const {user} = state;
+</script>
+
+<template>
+	<div>Bonjour {{ user.name }} </div>
+	<div>Age: {{ user.age }} </div>
+  <button @click="toggleUserInfo">Toggle User State</button>
+	<div>Count: {{ state.count }}</div>
+	<button @click="incrementCount">UP</button>
+	<button @click="decreaseCount">Down</button>
+</template>
+
+<style scoped>
+
+</style>
